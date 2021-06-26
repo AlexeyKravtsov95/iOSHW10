831121bdac5130d27b9fd6fe015952fea741bd05
kravtsov <al.kravtsov@cft.ru>
2021-06-26 16:11:57 +0700

    Initial Commit

---
+++ iOSHW10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSHW10
+//
+//  Created by Кравцов Алексей Игоревич on 26.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
